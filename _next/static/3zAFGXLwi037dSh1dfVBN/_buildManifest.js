713323c7a59be04868d5fc49728c893f819a1b78
okvianey <vianymarcelo@gmail.com>
2025-11-08 13:12:11 -0600

    Updates

--- _next/static/3zAFGXLwi037dSh1dfVBN/_buildManifest.js
+++ _next/static/3zAFGXLwi037dSh1dfVBN/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/flatlatte-next//_next/:path+",
-        "destination": "/flatlatte-next/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
