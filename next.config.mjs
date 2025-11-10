436b3b7c50dff6388a9164ef72f8e26fcc2cc1f2
okvianey <vianymarcelo@gmail.com>
2025-11-10 09:55:22 -0600

    actualizar next 15 y react 18

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
-  reactCompiler: true,
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '' : '', // Para custom domain puede estar vacío
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
   images: {
     unoptimized: true
-  }
+  },
 };
 
 export default nextConfig;
